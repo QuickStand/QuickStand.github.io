35cc1a2b10f7d6887017df3b770b62eaacd77d2c
QuickStand <quickstand.asso@gmail.com>
2023-05-15 18:28:33 +0200

    added hfs18

--- ranking/ranking.js
+++ ranking/ranking.js
@@ -1,7 +1,7 @@
 
 // all tournaments
 // ordered from most recent to oldest
-const tournaments = [hfs23, mixup22, hfs22, stunfest22, judgement2, pbn3, hfsreborn, judgement1, saf19, hfs19, stunfest19, pbn2, pbn1];
+const tournaments = [hfs23, mixup22, hfs22, stunfest22, judgement2, pbn3, hfsreborn, judgement1, saf19, hfs19, stunfest19, pbn2, hfs18, pbn1];
 
 // non year-adjusted score of a player at a tournament
 function base_score_player_tournament (player_name, tournament) {
